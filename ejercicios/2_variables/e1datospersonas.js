6b82cc0d60e27cb3319ed720fac4ccb83a5869c4
Janira Huesca <janirahuesca@gmail.com>
2024-11-18 19:53:18 +0000

    feat(2_variables): e1datospersonales
    Realizado ejercicio 1 del apartado Variables

---
+++ ejercicios/2_variables/e1datospersonas.js
@@ -0,0 +1,45 @@
+// Declaración de variables y asignación de valores con datos sobre mi:
+let nombre = "Janira";
+let apellidos = "Huesca Quinto";
+let edad = 37;
+let altura = 1.69;
+let ciudad = "Banyoles";
+let gustaPlaya = true;
+let gustaMontaña = true;
+let gustaChocolate = true;
+let tieneMascota = true;
+
+// Mostrar la información sobre mi:
+document.write("INFORMACIÓN SOBRE MI:" + "<br>");
+document.write("Nombre: " + nombre + "<br>");
+document.write("Apellidos: " + apellidos + "<br>");
+document.write("Edad: " + edad + "<br>");
+document.write("Altura: " + altura + "<br>");
+document.write("Ciudad: " + ciudad + "<br>");
+document.write("¿Me gusta la playa?: " + gustaPlaya + "<br>");
+document.write("¿Me gusta la montaña?: " + gustaMontaña + "<br>");
+document.write("¿Me gusta el chocolate?: " + gustaChocolate + "<br>");
+document.write("¿Tengo mascota?: " + tieneMascota + "<br>");
+
+// Asignación de nuevos valores con datos sobre mi pareja:
+nombre = "Jordi";
+apellidos = "Coll Bordas";
+edad = 35;
+altura = 1.87;
+ciudad = "Banyoles";
+gustaPlaya = false;
+gustaMontaña = true;
+gustaChocolate = true;
+tieneMascota = true;
+
+// Mostrar la información sobre mi pareja:
+document.write("INFORMACIÓN SOBRE MI PAREJA:" + "<br>");
+document.write("Nombre: " + nombre + "<br>");
+document.write("Apellidos: " + apellidos + "<br>");
+document.write("Edad: " + edad + "<br>");
+document.write("Altura: " + altura + "<br>");
+document.write("Ciudad: " + ciudad + "<br>");
+document.write("¿Le gusta la playa?: " + gustaPlaya + "<br>");
+document.write("¿Le gusta la montaña?: " + gustaMontaña + "<br>");
+document.write("¿Le gusta el chocolate?: " + gustaChocolate + "<br>");
+document.write("¿Tengo mascota?: " + tieneMascota + "<br>");
