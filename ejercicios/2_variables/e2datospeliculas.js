bcba340e35f53a540e48a1e797be43498b3d6ff8
Janira Huesca <janirahuesca@gmail.com>
2024-11-18 20:13:06 +0000

    feat(2_variables): e2datospeliculas
    Realizado ejercicio 2 del apartado Variables

---
+++ ejercicios/2_variables/e2datospeliculas.js
@@ -0,0 +1,33 @@
+// Declaración de variables y asignación de valores con datos una película:
+let titulo = "Atrapado en el tiempo";
+let director = "Harold Ramis";
+let anoLanzamiento = 1993;
+let genero = "Fantástico";
+let duracion = 101;
+let tienePremios = true;
+
+// Mostrar la información la película:
+document.write("INFORMACIÓN SOBRE LA PELÍCULA:" + "<br>");
+document.write("Título de la película: " + titulo + "<br>");
+document.write("Director: " + director + "<br>");
+document.write("Año de lanzamiento: " + anoLanzamiento + "<br>");
+document.write("Género: " + genero + "<br>");
+document.write("Duración: " + duracion + " minutos" + "<br>");
+document.write("¿Tiene premios?: " + tienePremios + "<br>");
+
+// Asignación de nuevos valores con datos en inglés:
+titulo = "Groundhog day";
+director = "Harold Ramis";
+anoLanzamiento = 1993;
+genero = "Fantasy";
+duracion = "101";
+tienePremios = true;
+
+// Mostrar la información con los datos en inglés:
+document.write("INFORMACIÓN SOBRE LA PELÍCULA EN INGLÉS:" + "<br>");
+document.write("Film title: " + titulo + "<br>");
+document.write("Director: " + director + "<br>");
+document.write("Year: " + anoLanzamiento + "<br>");
+document.write("Genre: " + genero + "<br>");
+document.write("Duration: " + duracion + " minutes" + "<br>");
+document.write("Has awards?: " + tienePremios + "<br>");
