dfc88e94b0257e7f4a174fed3dc6202440901d1d
Janira Huesca <janirahuesca@gmail.com>
2024-11-19 16:01:03 +0000

    feat(2_variables): e4datoslibros
    Realizado ejercicio 4 del apartado Variables

---
+++ ejercicios/2_variables/e4datoslibros.js
@@ -0,0 +1,33 @@
+// Declaración de variables y asignación de valores con datos sobre mi libro favorito:
+let titulo = "Saber perder";
+let autor = "David Trueba";
+let genero = "Drama";
+let anoPublicacion = 2019;
+let numeroPaginas = 528;
+let tienePelicula = false;
+
+// Mostrar la información de mi libro favorito:
+document.write("INFORMACIÓN SOBRE MI LIBRO FAVORITO:" + "<br>");
+document.write("Título del libro: " + titulo + "<br>");
+document.write("Autor: " + autor + "<br>");
+document.write("Género: " + genero + "<br>");
+document.write("Año de publicación: " + anoPublicacion + "<br>");
+document.write("Número de páginas: " + numeroPaginas + "<br>");
+document.write("¿Tiene película?: " + tienePelicula + "<br>");
+
+// Asignación de nuevos valores con datos sobre libro de Harry Potter:
+titulo = "Harry Potter y la piedra filosofal";
+autor = "J.K. Rowling";
+genero = "Fantástico";
+anoPublicacion = 1999;
+numeroPaginas = 264;
+tienePelicula = true;
+
+// Mostrar la información de libro de Harry Potter:
+document.write("INFORMACIÓN SOBRE EL LIBRO DE HARRY POTTER:" + "<br>");
+document.write("Título del libro: " + titulo + "<br>");
+document.write("Autor: " + autor + "<br>");
+document.write("Género: " + genero + "<br>");
+document.write("Año de publicación: " + anoPublicacion + "<br>");
+document.write("Número de páginas: " + numeroPaginas + "<br>");
+document.write("¿Tiene película?: " + tienePelicula + "<br>");
