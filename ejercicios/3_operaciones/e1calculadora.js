45edb8b29fecebdd444df83d43e92e26198ada4e
Janira Huesca <janirahuesca@gmail.com>
2024-11-19 16:20:03 +0000

    feat(3_operaciones): e1calculadora
    Realizado ejercicio 1 del apartado Operaciones

---
+++ ejercicios/3_operaciones/e1calculadora.js
@@ -0,0 +1,16 @@
+// Declaración de variables y asignación de valores a los dos operadores:
+let numero1 = 10;
+let numero2 = 4;
+
+// Realización de las operaciones:
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+// Mostrar la información de mi libro favorito:
+document.write("RESULTADO CALCULADORA CON OPERACIONES BÁSICAS:" + "<br>");
+document.write("Suma: El resultado de sumar " + numero1 + " y " + numero2 + " es " + suma + "<br>");
+document.write("Resta: El resultado de restar " + numero1 + " y " + numero2 + " es " + resta + "<br>");
+document.write("Multiplicación: El resultado de multiplicar " + numero1 + " y " + numero2 + " es " + multiplicacion + "<br>");
+document.write("División: El resultado de dividir " + numero1 + " y " + numero2 + " es " + division + "<br>");
