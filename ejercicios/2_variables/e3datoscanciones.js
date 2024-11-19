b3e108b0a0d88906c5dfe565919935beaed40d07
Janira Huesca <janirahuesca@gmail.com>
2024-11-19 15:47:51 +0000

    feat(2_variables): e3datoscanciones
    Realizado ejercicio 3 del apartado Variables

---
+++ ejercicios/2_variables/e3datoscanciones.js
@@ -0,0 +1,33 @@
+// Declaración de variables y asignación de valores con datos de mi canvión favorita:
+let titulo = "Haz de luz";
+let artista = "Rayden";
+let album = "Sinónimo";
+let anoLanzamiento = 2019;
+let duracion = 4.33;
+let tieneVideoclip = true;
+
+// Mostrar la información la canción:
+document.write("INFORMACIÓN SOBRE MI CANCIÓN FAVORITA:" + "<br>");
+document.write("Título de la canción: " + titulo + "<br>");
+document.write("Arista: " + artista + "<br>");
+document.write("Género: " + album + "<br>");
+document.write("Año de lanzamiento: " + anoLanzamiento + "<br>");
+document.write("Duración: " + duracion + " minutos" + "<br>");
+document.write("¿Tiene videoclip?: " + tieneVideoclip + "<br>");
+
+// Asignación de nuevos valores con datos de la canción que menos me gusta:
+titulo = "Aserejé";
+artista = "Las Ketchup";
+album = "Hijas del tomate";
+anoLanzamiento = 2002;
+duracion = 3.33;
+tieneVideoclip = true;
+
+// Mostrar la información con datos de la canción que menos me gusta:
+document.write("INFORMACIÓN SOBRE LA CANCIÓN QUE MENOS ME GUSTA:" + "<br>");
+document.write("Título de la canción: " + titulo + "<br>");
+document.write("Arista: " + artista + "<br>");
+document.write("Género: " + album + "<br>");
+document.write("Año de lanzamiento: " + anoLanzamiento + "<br>");
+document.write("Duración: " + duracion + " minutos" + "<br>");
+document.write("¿Tiene videoclip?: " + tieneVideoclip + "<br>");
